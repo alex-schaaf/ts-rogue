e52585df2841948661def2e611ac4fad6232f5b4
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-10-12 19:24:26 +0200

    Refactor map generation logic to include walls

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,23 @@ function generateMap(width: number, height: number): Record<string, Tile> {
     let map = {}
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
-            map[`${x},${y}`] = {
-                char: '.',
-                colorFg: '#404040',
-                colorBg: '#000',
-                isWalkable: true,
+            if (Math.random() < 0.2) {
+                map[`${x},${y}`] = {
+                    char: '#',
+                    colorFg: '#efefef',
+                    colorBg: '#000',
+                    isWalkable: false,
+                }
+
+            } else {
+                map[`${x},${y}`] = {
+                    char: '.',
+                    colorFg: '#404040',
+                    colorBg: '#000',
+                    isWalkable: true,
+                }
             }
+
         }
     }
     return map
